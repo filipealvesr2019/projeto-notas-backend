3ba19336f2b70ba4c26c916507fb5291142bd537
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-01-27 15:47:21 -0300

    criando autenticação

--- index.js
+++ index.js
@@ -1,103 +1,119 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bcrypt = require('bcrypt')
+const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const Usuario = require('./models/Usuario')
 
 app.use(express.json());
 app.use((req, res, next) => {
-    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`)
-    next()
-})
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+});
 
 // Rota que lança um erro
 
+app.get("/erro", (req, res) => {
+  throw new Error("Erro simulado!");
+});
 
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  res.status(500).send("Algo deu errado no banco de dados");
+});
 
-app.get('/erro', (req, res) => {
-    throw new Error('Erro simulado!');
-})
-
-app.use((err,req, res, next) => {
-    console.log(err.stack);
-    res.status(500).send('Algo deu errado no banco de dados')
-})
-
-app.get('/', (req, res) => {
-    res.send('Servidor Express.js esta rodando')
+app.get("/", (req, res) => {
+  res.send("Servidor Express.js esta rodando");
 });
 const protegerRota = (req, res, next) => {
-    const token = req.headers['authorization'];
-    if(token === "token-de-acesso"){
-        next()
-
-    }else{
-        res.status(403).send('Acesso negado token invalido!')
-    }
-}
-app.get('/rota-protegida', protegerRota, (req, res) => {
-    res.send('Voce acessou uma rota protegida')
-})
-
-const  logProdutoMiddleware = (req, res, next) => {
-    console.log('Produto foi acessado....')
-    next()
-
-}
-
-app.get('/produtos', logProdutoMiddleware,  (req, res) => {
-    res.send('Lista de Produtos');
+  const token = req.headers["authorization"];
+  if (token === "token-de-acesso") {
+    next();
+  } else {
+    res.status(403).send("Acesso negado token invalido!");
+  }
+};
+app.get("/rota-protegida", protegerRota, (req, res) => {
+  res.send("Voce acessou uma rota protegida");
 });
 
-app.get('/sobre', (req, res) => {
-    res.send('Pagina sobre Nos');
-})
+const logProdutoMiddleware = (req, res, next) => {
+  console.log("Produto foi acessado....");
+  next();
+};
+
+app.get("/produtos", logProdutoMiddleware, (req, res) => {
+  res.send("Lista de Produtos");
+});
+
+app.get("/sobre", (req, res) => {
+  res.send("Pagina sobre Nos");
+});
 
 // http://localhost:3000/adicionar-produto
 
-app.post('/adicionar-produto', (req, res) => {
-    const produto = req.body;
-    if(!produto.nome || !produto.preco){
-        console.log('campos sao necessarios ')
-    }
-    try{
-        res.send(`Produto ${produto.nome} adicionado com sucesso!`)
+app.post("/adicionar-produto", (req, res) => {
+  const produto = req.body;
+  if (!produto.nome || !produto.preco) {
+    console.log("campos sao necessarios ");
+  }
+  try {
+    res.send(`Produto ${produto.nome} adicionado com sucesso!`);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-    } catch(error){
-        console.log(error)
-    }
-})
+app.post("/criar-usuario", async (req, res) => {
+  const { nome, email, senha } = req.body;
 
-app.post('/criar-usuario', async (req, res) => {
-    const {nome, email, senha} = req.body;
+  try {
+   // gera o salt
+   
 
-})
+    const senhaHash = await bcrypt.hash(senha, 10);
+
+    const novoUsuario = new Usuario({
+      nome,
+      email,
+      senha: senhaHash,
+    });
+
+    await novoUsuario.save();
+
+    res.status(201).send({ message: "Usuario criado com sucesso!" });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+
 app.use((req, res) => {
-    res.status(404).send('Página não encontrada!')
-})
+  res.status(404).send("Página não encontrada!");
+});
 
 const uri = process.env.MONGODB_URI;
 
 if (!uri) {
-    console.error("Erro: A variável de ambiente MONGODB_URI não está definida.");
-    process.exit(1); // Encerra o processo, pois a conexão com o banco é crítica
-  }
+  console.error("Erro: A variável de ambiente MONGODB_URI não está definida.");
+  process.exit(1); // Encerra o processo, pois a conexão com o banco é crítica
+}
 const options = {
-    serverSelectionTimeoutMS: 30000, // 30 segundos
-    socketTimeoutMS: 30000, // 30 segundos
-  };
-  // Conexão com o banco de dados
-  mongoose
-    .connect(uri, options)
-    .then(() => {
-      console.log("Conectado ao banco de dados");
-    })
-    .catch((error) => {
-      console.error("Erro de conexão com o banco de dados:", error);
-    });
+  serverSelectionTimeoutMS: 30000, // 30 segundos
+  socketTimeoutMS: 30000, // 30 segundos
+};
+// Conexão com o banco de dados
+mongoose
+  .connect(uri, options)
+  .then(() => {
+    console.log("Conectado ao banco de dados");
+  })
+  .catch((error) => {
+    console.error("Erro de conexão com o banco de dados:", error);
+  });
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
-    console.log(`Servidor esta rodando em http://localhost:${PORT}`)
+  console.log(`Servidor esta rodando em http://localhost:${PORT}`);
 });
--- models/Usuario.js
+++ models/Usuario.js
@@ -4,6 +4,8 @@ const usuarioSchema = new mongoose.Schema({
     nome: {type: String, required: true},
     email: {type: String, required: true, unique: true},
     senha: {type: String, required: true},
-}, {timestamp: true});
+});
 
-const Usuarios = mongoose.model("Usuario", usuarioSchema)
+const Usuario = mongoose.model("Usuario", usuarioSchema)
+
+module.exports = Usuario;
