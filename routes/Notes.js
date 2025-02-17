f5aa1139243603009d5e2636e0700010cb3ef3e7
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-02-17 15:32:48 -0300

    projeto de nots simples parte 4

--- routes/Notes.js
+++ routes/Notes.js
@@ -1,3 +1,16 @@
 const express = require('express');
 const {body, validationResult} = require('express-validator');
 const sanitizeHtml = require('sanitize-html');
+const AuthMiddleware = require('../middleware/AuthMiddleware');
+const router = express.Router();
+
+// criar nota
+router.post('/', AuthMiddleware, [
+    body('titulo').trim().escape(),
+    body('conteudo'). customSanitizer(value => sanitizeHtml(value))
+], async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array()})
+    }
+})
