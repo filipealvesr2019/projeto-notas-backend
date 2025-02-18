5549608cc28e660e6e1aa7b6940a6e862f2fbdf8
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-02-18 15:51:51 -0300

    projeto de nots simples parte 4

--- routes/Notes.js
+++ routes/Notes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const {body, validationResult} = require('express-validator');
 const sanitizeHtml = require('sanitize-html');
 const AuthMiddleware = require('../middleware/AuthMiddleware');
+const Notas = require('../models/Notas');
 const router = express.Router();
 
 // criar nota
@@ -12,5 +13,12 @@ router.post('/', AuthMiddleware, [
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         return res.status(400).json({ errors: errors.array()})
+    }
+    const { titulo, conteudo } = req.body;
+    try {
+        const novaNota = new Notas({titulo, conteudo, userID: req.user.id});
+        await novaNota.save();
+    }catch(error){
+        console.log(error)
     }
 })
