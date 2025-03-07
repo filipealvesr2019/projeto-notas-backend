d41ea3beb4ea66dfc251a8f4d07a60cdc78df721
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-03-07 15:03:23 -0300

    projeto de nots simples parte 12

--- models/Notas.js
+++ models/Notas.js
@@ -2,7 +2,8 @@ const { default: mongoose } = require("mongoose");
 
 const NotasSchema = new mongoose.Schema({
     titulo: { type: String, required: true },
-    conteudo: { type: String, required: true}
+    conteudo: { type: String, required: true},
+    userID: { type: String, required: true},
 });
 
 module.exports =  mongoose.model('Notas', NotasSchema)
--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const { default: mongoose } = require("mongoose");
 const UserSchema = new mongoose.Schema({
     nome: { type: String, required: true },
     email: { type: String, required: true, unique: true},
-    senha: {type: String, required: true}
+    senha: {type: String, required: true},
 });
 
 module.exports =  mongoose.model('User', UserSchema)
