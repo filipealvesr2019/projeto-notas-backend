f22d645fcc04771d74f5b6016c2fdee2e607e756
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-03-11 15:32:44 -0300

    Upload de Arquivos com Multer e Segurança

--- middleware/uploadMiddleware.js
+++ middleware/uploadMiddleware.js
@@ -5,5 +5,13 @@ const path = require("path");
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, 'uploads/'); // pasta aonde os arquivos serao salvos
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${Data.now()}-${file.originalname}`);
     }
-})
+});
+
+// Filtra pra os tipos de arquivos permitidos
+const fileFilter = (req, file, cb) => {
+    const alowedTypes = /jpeg|jpg|png|pdf/
+}
