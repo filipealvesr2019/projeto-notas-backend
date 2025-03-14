9c8384ea01e84e10aa2d5319ed22b7a43593425b
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-03-14 14:59:50 -0300

    Upload de Arquivos com Multer e Segurança

--- routes/uploads.js
+++ routes/uploads.js
@@ -3,4 +3,15 @@ const uploadMiddleware = require('../middleware/uploadMiddleware');
 const router = express.Router();
 
 // upload arquivos com autenticação
-router.post('/upload', uploadMiddleware, )
+router.post('/upload', uploadMiddleware, upload.single('arquivo'), (req, res) => {
+    if(!req.file){
+        return res.status(400).json({ error: 'Nenhum arquivo enviado!'})
+    }
+
+    res.status(200).json({
+        message: "Arquivo enviado com sucesso!",
+        file: req.file
+    })
+});
+
+module.exports = router;
