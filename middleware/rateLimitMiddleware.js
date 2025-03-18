455d9ae784d97a4e489b3032879bdc64da8981c8
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-03-18 14:17:54 -0300

    Upload de Arquivos com Multer e Segurança

---
+++ middleware/rateLimitMiddleware.js
@@ -0,0 +1,12 @@
+const ratelimit = require('express-rate-limit');
+
+// limite de 100 requesiçoes por ip a cada 15 minutos
+
+const limiter = ratelimit({
+    window: 15 * 60 * 1000, // 15 minutos
+    max: 100,
+    message: {error: "Muitas requisições, tente novamente mais tarde."}
+
+});
+
+module.exports = limiter
